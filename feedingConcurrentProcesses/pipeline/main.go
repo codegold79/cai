786b3b49cea0ef42b779cc9107373b08595284ec
codegold79 <codegold79@gmail.com>
2020-01-11 15:36:59 -0800

    Solution to channel problem

--- feedingConcurrentProcesses/pipeline/main.go
+++ feedingConcurrentProcesses/pipeline/main.go
@@ -16,37 +16,41 @@ func main() {
 		width = 2
 	)
 
-	ssc := make(chan string)
+	ssc := make(chan string) // Producer sends messages through the ssc.
 	go func() {
-		// .
+		defer close(ssc)
+
 		for _, s := range data() {
 			ssc <- s
 		}
 	}()
 
+
 	infos := make(chan *Info)
-	// ..
 
-	var wg sync.WaitGroup
-	wg.Add(width)
-	for iter := 0; iter < width; iter++ {
-		go func(n int) {
-			defer wg.Done()
-			for s := range ssc {
-				i := &Info{
-					routine: n,
-					s:       strings.ToUpper(s),
+	go func() { // consumer
+		defer close(infos)
+
+		var wg sync.WaitGroup
+		wg.Add(width)
+
+		for iter := 0; iter < width; iter++ {
+			go func(n int) { // digester
+				defer wg.Done()
+
+				for s := range ssc {
+					i := &Info{
+						routine: n,
+						s:       strings.ToUpper(s),
+					}
+					infos <- i
 				}
-				infos <- i
-			}
-		}(iter)
-	}
+			}(iter)
+		}
 
-	wg.Wait()
-	// The problem is that infos channel can't be filled above for loop because
-	// there's no one there to take from the channel (below). But code below won't
-	// happen because we're asking them to wg.Wait(). 
-	// Channels are a way to signal to wait, but Done will need to be added.
+		wg.Wait()
+	}()
+
 	for i := range infos {
 		fmt.Println(i)
 	}
